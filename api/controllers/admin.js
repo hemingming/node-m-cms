515d520fddb5acccfedf0697382e151b6e998a53
何明明 <mingming.he@bl.com>
2016-12-15 17:42:11 +0800

    git add :alien:

--- api/controllers/admin.js
+++ api/controllers/admin.js
@@ -3,13 +3,77 @@
 module.exports = {
     adminRoutes : function(app){
 
-        app.get('/nodemalladministrator', this.adminMain);
+        //系统首页
+        app.get('/admin&malladministrator*', this.adminAdministrator);
 
+        //系统信息
+        app.get('/admin&adminmain*', this.adminMain);
+
+        //商品管理
+        app.get('/admin&mallgoods*', this.adminMallgoods);
+        app.get('/admin&pubgoods*', this.adminPubgoods);
+
+        //订单管理
+        app.get('/admin&ordersales*', this.adminOrdersales);
+        app.get('/admin&orderpayment*', this.adminOrderpayment);
+
+        //消息管理
+        app.get('/admin&news*', this.adminNews);
+        app.get('/admin&pubnews*', this.adminPubnews);
+
+    },
+
+
+    adminAdministrator : function(req, res, next){
+        res.render('admin/home', {
+            layout : 'admin'
+        });
     },
 
     adminMain : function(req, res, next){
         res.render('admin/home', {
-            layout : 'admin'
+            layout : 'adminmain'
         });
+    },
+
+
+    adminMallgoods : function(req, res, next){
+        res.render('admin/mallgoods', {
+            layout : 'adminmain'
+        })
+    },
+
+    adminPubgoods : function(req, res, next){
+        res.render('admin/pubmallgoods', {
+            layout : 'adminmain'
+        })
+    },
+
+
+    adminOrdersales : function(req, res, next){
+        res.render('admin/ordersales', {
+            layout : 'adminmain'
+        })
+    },
+
+    adminOrderpayment : function(req, res, next){
+        res.render('admin/orderpayment', {
+            layout : 'adminmain'
+        })
+    },
+
+
+    adminNews : function(req, res, next){
+        res.render('admin/mallgoods', {
+            layout : 'adminmain'
+        })
+    },
+
+    adminPubnews : function(req, res, next){
+        res.render('admin/mallgoods', {
+            layout : 'adminmain'
+        })
     }
+
+
 }
